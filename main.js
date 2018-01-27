2ed8f6a13ec3f1b298b172c7e53f729220980761
Kevin Leutzinger <me@kevinleutzinger.com>
2018-01-27 11:47:12 -0800

    ensure script runs more consistently on navigation

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-$('#player').find('*').focus((e) => {
-  $(e.target).blur();
+
+function run (debugStr) {
+  console.log('ran keyblocker on ' + debugStr);
+  $('#player').find('*').focus((e) => {
+    $(e.target).blur();
+  });
+}
+
+run('pageload');
+
+$( document ).ready(function() {
+  run('docready');
+})
+
+window.addEventListener('yt-navigate-finish', function (event) {
+  run('new navigation');
+});
+
+window.addEventListener('spfdone', function (e) {
+  run('old navigation');
 });
